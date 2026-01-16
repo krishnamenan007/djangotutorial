1c4d5403a96797e7ccf1a8f9375e2da77b8c8f49
Krishnahuex28 <krishnamenan007@gmail.com>
2026-01-16 15:09:01 +0530

    Revert "postgresql dockerization"
    This reverts commit e84548bda502a9c07a6753910a3499eafe07a398.

--- myfirstsite/settings.py
+++ myfirstsite/settings.py
@@ -11,11 +11,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,12 +76,8 @@ WSGI_APPLICATION = 'myfirstsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'djangotutorial'),
-        'USER': os.getenv('DB_USER', 'django_user'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'django_password'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
